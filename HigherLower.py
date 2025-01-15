bb9ebdcfa97cad2e9c35d70002bce74c82ddbccd
rigellin73 <rigellin73@gmail.com>
2025-01-15 18:11:33 +0100

    create separate function for checking answer

--- HigherLower.py
+++ HigherLower.py
@@ -20,6 +20,12 @@ def print_result(has_won, final_score):
 def get_output_str(item):
     return f"{item['name']}, {item['description']}, from {item['country']}"
 
+def answer_is_correct(player_guess, first_count, second_count):
+    if first_count > second_count:
+        return player_guess == "A"
+    else:
+        return player_guess == "B"
+
 def game():
     print_logo()
     score = 0
@@ -28,14 +34,10 @@ def game():
     player_won = True
     while data:
         second_item = data.pop()
-        winner = "B"
-        if first_item['follower_count'] > second_item['follower_count']:
-            winner = "A"
-
         print(f"Compare A: {get_output_str(first_item)}")
         print(f"Against B: {get_output_str(second_item)}")
-        player_choice = input("Who has more followers? Type 'A' or 'B': ")
-        if player_choice == winner:
+        guess = input("Who has more followers? Type 'A' or 'B': ")
+        if answer_is_correct(guess, first_item['follower_count'], second_item['follower_count']):
             score += 1
             first_item = second_item
             print_intermediate_result(score)
