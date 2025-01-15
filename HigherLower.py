84a28f6c2602b414c82c0fc57288f5426a7f2899
rigellin73 <rigellin73@gmail.com>
2025-01-15 16:58:25 +0100

    add input and win check

--- HigherLower.py
+++ HigherLower.py
@@ -1,5 +1,23 @@
+from random import sample
 from ascii_art import logo
 from participants_dictionary import participants
 
 print(logo)
-print(participants)
+
+def game():
+    while True:
+        competitors = sample(list(participants.keys()), 2)
+        winner = "B"
+        if participants[competitors[0]] > participants[competitors[1]]:
+            winner = "A"
+
+        print(f"Compare A: {competitors[0]}")
+        print(f"Against B: {competitors[1]}")
+        player_choice = input("Who is heavier? Type 'A' or 'B': ")
+        if player_choice == winner:
+            print("Correct!")
+        else:
+            print("Nope!")
+            break
+
+game()
