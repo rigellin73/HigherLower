ef87da24e003cd135c1e9b03c9d6f6fbaaa67a1f
rigellin73 <rigellin73@gmail.com>
2025-01-15 17:39:46 +0100

    move output to diferent functions

--- HigherLower.py
+++ HigherLower.py
@@ -2,13 +2,28 @@ from random import shuffle
 from ascii_art import logo
 from animal_weights_dictionary import animal_weights_dict
 
-print(logo)
+def print_logo():
+    print("\n" * 20)
+    print(logo)
+
+def print_intermediate_result(current_score):
+    print_logo()
+    print(f"You right! Your current score is: {current_score}")
+
+def print_result(has_won, final_score):
+    if has_won:
+        print("Congratulations! You guessed all.")
+    else:
+        print("Sorry, that's wrong.")
+    print(f"Your final score is: {final_score}")
 
 def game():
+    print_logo()
     score = 0
     animals = list(animal_weights_dict.keys())
     shuffle(animals)
     first_animal = animals.pop()
+    player_won = True
     while animals:
         second_animal = animals.pop()
         winner = "B"
@@ -19,13 +34,13 @@ def game():
         print(f"Against B: {second_animal}")
         player_choice = input("Who is heavier? Type 'A' or 'B': ")
         if player_choice == winner:
-            print("Correct!")
             score += 1
             first_animal = second_animal
-            print(f"Your current score is: {score}")
+            print_intermediate_result(score)
         else:
-            print(f"Nope!")
+            player_won = False
             break
-    print(f"Your final score is: {score}")
+    print_logo()
+    print_result(player_won, score)
 
 game()
